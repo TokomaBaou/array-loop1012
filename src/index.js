253970f08c8a309cd4d420ea8fc665377ca5395a
MAKOTO OBA <mac.kageyama@gmail.com>
2022-10-12 15:13:30 +0000

    Update 13 files

--- src/index.js
+++ src/index.js
@@ -8,3 +8,10 @@ document.getElementById("app").innerHTML = `
   <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
 </div>
 `;
+
+for (let i = 1; i <= 10; i++) {
+  if (i === 5) {
+    continue;
+  }
+  console.log(`${i}回目のループ処理`);
+}
